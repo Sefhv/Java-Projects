956d944acd696e136c934f922d10b1ad38c5876d
Sefhv <43103433+Sefhv@users.noreply.github.com>
2020-04-24 15:03:55 -0500

    Add todo methods
    1. Add todo resources Rest methods

--- restfull-web-services/src/main/java/com/user/rest/webservices/restfullwebservices/todo/TodoHardcodedService.java
+++ restfull-web-services/src/main/java/com/user/rest/webservices/restfullwebservices/todo/TodoHardcodedService.java
@@ -3,7 +3,6 @@ package com.user.rest.webservices.restfullwebservices.todo;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
-
 import org.springframework.stereotype.Service;
 
 @Service
@@ -15,11 +14,41 @@ public class TodoHardcodedService {
 	static {
 		todos.add(new Todo(++idCounter, "sergio", "Learn to Dance", new Date(), false));
 		todos.add(new Todo(++idCounter, "sergio", "Learn about Microservices", new Date(), false));
-		todos.add(new Todo(++idCounter, "sergio", "Learn about Angualar", new Date(), false));
+		todos.add(new Todo(++idCounter, "sergio", "Learn about Angular", new Date(), false));
 	}
 
 	public List<Todo> findAll() {
 		return todos;
+	}
+
+	public Todo saveTodo(Todo todo) {
+		if (todo.getId() == null) {
+			todo.setId(++idCounter);
+			todos.add(todo);
+		} else {
+			deleteById(todo.getId());
+			todos.add(todo);
+		}
+		return todo;
+	}
+
+	public Todo deleteById(Long id) {
+		Todo todo = findById(id);
+
+		if (todo.getId() == null)
+			return new Todo();
+
+		if (todos.remove(todo))
+			return todo;
+
+		return new Todo();
+	}
+
+	public Todo findById(Long id) {
+		for (Todo todo : todos)
+			if (todo.getId().equals(id))
+				return todo;
+		return new Todo();
 	}
 
 }
